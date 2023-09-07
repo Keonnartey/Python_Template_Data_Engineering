5d4ca9c0d6400b7c924c6faaf634eb6a6771c7ac
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-07 05:09:32 +0000

    trying to work on the pandas dataframe

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def get_average(l):
-    return sum(l)/len(l)
+import pandas as pd
+import numpy as np
+
+car = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
+#print(car.head())
+#print(car["mpg"])
+
+def max(x) -> float:
+    x = car["mpg"].max()
+    return x
+
+max()
