2976a596f107420b1baf1525b6ea6c751a449589
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-11 12:28:20 -0700

    added pandas import

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from main import max_cars
+import pandas as pd
 
 def test_max():
     cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
