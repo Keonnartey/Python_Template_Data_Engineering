3038a60ce1fb837369bf0af4b95177b4af0c36d2
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-11 12:38:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def max_cars(df: pd.DataFrame, col: str) -> float:
 
 if __name__ == '__main__':
     car = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
-    print(cars.head())
+    print(car.head())
 
