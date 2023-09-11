0fcfdc08b1c1f9671f3b6232b4bb618515d93619
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-11 12:35:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def max_cars(df: pd.DataFrame, col: str) -> float:
-    return = df[col].max()
+    return df[col].max()
 
 if __name__ == '__main__':
     car = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
