2741f14733e37b33b245477f3cdd4b47cb406534
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-11 12:27:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import numpy as np
 
 car = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
 #print(car.head())
 #print(car["mpg"])
 
-def max_cars(x) -> float:
+def max_cars(car) -> float:
     x = car["mpg"].max()
     return x
 
