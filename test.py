7c0d9e1b6ea02478b0702652f08682eb6dcf8c3a
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-09-11 12:30:10 -0700

    changed the max cars

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pandas as pd
 def test_max():
     cars = pd.read_csv(r"https://gist.githubusercontent.com/seankross/a412dfbd88b3db70b74b/raw/5f23f993cd87c283ce766e7ac6b329ee7cc2e1d1/mtcars.csv")
     target_column = "mpg"
-    max_mpg = pandas_descriptive_stat_mean(cars, target_column)
+    max_mpg = max_cars(cars, target_column)
 
     max_mpg = max_cars(cars, target_column)
     calculated_max = cars[target_column].max()
